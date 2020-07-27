f69934c4cc51c106506aca7db3ca443d9c198d6f
Araguntajyothi <Aragunta.Jyothi@cognizant.com>
2020-07-27 14:23:00 +0530

    COMMIT

---
+++ springbootrests1/src/main/java/com/example/demo/Emp.java
@@ -0,0 +1,62 @@
+package com.example.demo;
+
+import java.util.Date;
+
+public class Emp {
+	private long eID;
+	private String eName;
+	private String dptName;
+	private Date createdDate;
+	
+	public Emp() {
+		super();
+	}
+
+	public Emp(long eID, String eName, String dptName, Date createdDate) {
+		super();
+		this.eID = eID;
+		this.eName = eName;
+		this.dptName = dptName;
+		this.createdDate = createdDate;
+	}
+
+	
+
+	public Emp(int i, String string, String string2) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public long geteID() {
+		return eID;
+	}
+
+	public void seteID(long eID) {
+		this.eID = eID;
+	}
+
+	public String geteName() {
+		return eName;
+	}
+
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+
+	public String getDptName() {
+		return dptName;
+	}
+
+	public void setDptName(String dptName) {
+		this.dptName = dptName;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	
+
+}
